c9bc76bcd17e6c21be12dba53a97826cc686bf3c
Kai Diefenbach <kai.diefenbach@iqpp.de>
2016-02-16 08:10:56 +0100

    Updates messages.

--- lfs_canonical/views.py
+++ lfs_canonical/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from lfs.catalog.models import Product
 from lfs.core.utils import render_to_ajax_response
 from . models import Canonical
@@ -16,14 +17,17 @@ def edit_canonical(request, product_id):
         try:
             canonical = Canonical.objects.get(product=product)
         except Canonical.DoesNotExist:
+            message = u"Canonical wurde angelegt"
             if url != "":
                 Canonical.objects.create(product=product, url=request.POST.get("url"), kind=kind)
         else:
             if url != "":
+                message = u"Canonical wurde aktualisiert"
                 canonical.url = url
                 canonical.kind = kind
                 canonical.save()
             else:
+                message = u"Canonical wurde gelöscht"
                 canonical.delete()
 
-    return render_to_ajax_response(message="Hurz")
+    return render_to_ajax_response(message=message)
